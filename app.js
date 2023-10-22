a476bf779c5a295fa2f133b0e620bf0c11eb3d37
ProExecutor <123886692+ProExecutor@users.noreply.github.com>
2023-10-22 05:02:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-FROM node:10
-WORKDIR /app
-COPY package.json /app
-RUN npm install
-COPY . /app
-CMD node app.js
+const { exec } = require('child_process');
+
+let cmd = 'docker run -it -p 7860:7860 --platform=linux/amd64 --gpus all registry.hf.space/ap123-illusiondiffusion:gpu-8e6c97d python app.py';
+
+exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
